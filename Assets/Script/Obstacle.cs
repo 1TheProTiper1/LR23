a6d22634743c0b9432c03e2b2da9ff8934d351ad
1TheProTiper1 <93571164+1TheProTiper1@users.noreply.github.com>
2022-12-02 20:21:40 +0300

    1

---
+++ Assets/Script/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{  
+    void Start()
+    {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            FindObjectOfType<Player>().PlayerDead();
+        }
+    }
+    public void RemoveObstacle()
+    {
+        //ball hit hoot remove it
+        Destroy(gameObject);
+    }
+}
